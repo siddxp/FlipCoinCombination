58280457338eb2586095cf327242ae0ad49d16d4
siddxp <siddhantk071@gmail.com>
2020-07-21 15:25:35 +0530

    Singlet Percentage added

--- flipCoinCombo.sh
+++ flipCoinCombo.sh
@@ -1,8 +1,22 @@
 #!/bin/bash -x
-randomCoin=$(( RANDOM%2 ))
-if [ $randomCoin -eq 0 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A singlet
+echo "SINGLET"
+headsCount=0
+tailsCount=0
+
+for ((i=1;i<=10;i++))
+do
+
+	randomCoin=$(( RANDOM%2 ))
+	if [ $randomCoin -eq 0 ]
+	then
+		headsCount=$(($headsCount+1))
+	else
+		tailsCount=$(($tailsCount+1))
+	fi
+done
+
+headsPercentage=$(($headsCount*100/10))
+tailsPercentage=$(($tailsCount*100/10))
+echo "Singlet heads Percentage : $headsPercentage%"
+echo "Singlet tails Percentage : $tailsPercentage%"
